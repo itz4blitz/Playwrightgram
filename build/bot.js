fb95df8684c2a9f47ae8aad28902f8426ede92bf
Justin Scroggins <justinscroggins@outlook.com>
2022-08-23 15:17:00 -0400

    hotfix reporter

--- build/bot.js
+++ build/bot.js
@@ -21,9 +21,6 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.on("message", (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     ctx.reply("Got another message!");
 }));
-bot.command("start", (ctx) => {
-    ctx.replyWithAnimation("test");
-});
 // Now that you specified how to handle messages, you can start your bot.
 // This will connect to the Telegram servers and wait for messages.
 // Start the bot.
